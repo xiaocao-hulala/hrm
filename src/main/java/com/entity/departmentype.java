11231ac26e84a64bd53a61bf91a75833aaf7d115
cc <2116918787@qq.com>
2020-05-15 15:44:57 +0800

    添加了部门类型表实体类

---
+++ src/main/java/com/entity/departmentype.java
@@ -0,0 +1,12 @@
+package com.entity;
+
+/**
+ * @Author:caocong
+ * @Description:
+ * @Date:create in 15:41 2020/5/15
+ *//*
+*部门类型表
+create by caocong on  2020/5/15
+*/
+public class departmentype {
+}
